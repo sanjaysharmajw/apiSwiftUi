3bbfa861c5be5f8dddc1bf81fa91664913d9256a
Sanjay Sharma <58361466+sanjaysharmajw@users.noreply.github.com>
2024-07-17 14:15:53 +0530

    initial

---
+++ ApiSwiftUI/ApiSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiSwiftUIApp.swift
+//  ApiSwiftUI
+//
+//  Created by Sanjay Sharma on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ApiSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
