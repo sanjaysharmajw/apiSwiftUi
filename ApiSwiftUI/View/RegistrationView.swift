f5dec2c4dcf91870241a299799e640c8bdf125c9
Sanjay Sharma <58361466+sanjaysharmajw@users.noreply.github.com>
2024-07-17 18:30:39 +0530

    some changes

--- ApiSwiftUI/View/RegistrationView.swift
+++ ApiSwiftUI/View/RegistrationView.swift
@@ -6,7 +6,56 @@ struct RegistrationView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack {
+                // Header
+                HStack {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "arrow.left")
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    Text("Registration")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                       
+                    Spacer()
+                }
+                .padding()
+                .background(Color.blue)
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Member Name")
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+                    
+                    TextField("Member Name", text: $memberName)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                    
+                    Text("Mobile Number")
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+                    
+                    TextField("Mobile Number", text: $mobileNumber)
+                        .keyboardType(.numberPad)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                }
+                .padding(.horizontal, 24)
+                .padding(.top, 24)
+                
+                Spacer()
+                
+                CustomButton(title: "Submit") {
+                    
+                }.padding(.horizontal,24)
+            }
+        }
     }
 }
 
